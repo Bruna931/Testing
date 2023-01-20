8bf051e16116251434910e7cc0de813fea47fe21
Brunilda <brunillda.isaku@invenics.com>
2023-01-20 10:21:40 +0000

    new

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[400],
+      body: SafeArea(
+        child: Center(
+          child: Column(children: [
+            Text(
+              'Welcome Again',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            Text(
+              'Welcome back you have beem missed',
+              style: TextStyle(
+                fontSize: 28,
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
